504bd2f5f025ab6e3b39d87120799d7b812d252b
Greilesy <ryancheong07@gmail.com>
2025-04-03 14:21:57 +0800

    Removed unnecessary files from dataset.

---
+++ loaddata.py
@@ -0,0 +1,13 @@
+import os 
+import torch
+from torchvision import datasets, transforms
+
+
+transform = transforms.Compose([transforms.Resize(255),
+                                 transforms.ToTensor()])
+
+dataset = datasets.ImageFolder('Dataset', transform=transform)
+
+print(len(dataset))
+print(dataset.classes)
+
