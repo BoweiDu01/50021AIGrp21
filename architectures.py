9ab838e8bf1b72d126ef66d93d7327cf5fc6456c
Gregory <49094805+glimeuxe@users.noreply.github.com>
2025-04-16 05:45:36 +0000

    Add files via upload

--- architectures.py
+++ architectures.py
@@ -70,15 +70,24 @@ class R50ViTB16(nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.id = self.__class__.__name__
-		self.backbone = timm.create_model("vit_base_r50_s16_224", pretrained=True, num_classes=0)
-		self.pool = nn.Identity()
-		self.classifier = nn.Linear(self.backbone.num_features, 2)
+		self.model = timm.create_model("vit_base_r50_s16_224", pretrained=True, num_classes=2)
 
 	def forward(self, x):
-		x = self.backbone(x)
-		x = self.pool(x)
-		x = self.classifier(x)
-		return x
+		return self.model(x)
+
+class CLIPViTL14(nn.Module):
+	def __init__(self):
+		super().__init__()
+		self.id = self.__class__.__name__
+		model, _, preprocess = open_clip.create_model_and_transforms("ViT-L-14", pretrained="openai")
+		self.model = model
+		self.preprocess = preprocess
+		self.tokenizer = open_clip.get_tokenizer("ViT-L-14")
+		self.prompts = ["an image of a nonrecyclable item", "an image of a recyclable item"]
+		self.text_tokens = self.tokenizer(self.prompts).to(DEVICE)
+
+		for param in self.model.parameters():
+			param.requires_grad = True
 
 # ViT architectures
 class ViTB16(nn.Module):
@@ -98,6 +107,7 @@ class RN50(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
+		self.freezable = True
 		self.model = models.resnet50(pretrained=True)
 		self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
@@ -307,11 +317,21 @@ class MNV3L(nn.Module):
 		return self.model(x)
 
 # Experiments
+class CNV2B(nn.Module):
+	def __init__(self, num_classes=2):
+		super().__init__()
+		self.id = self.__class__.__name__
+		self.model = timm.create_model("convnextv2_base.fcmae_ft_in22k_in1k", pretrained=True)
+		self.model.head = nn.Linear(self.model.head.in_features, num_classes)
+
+	def forward(self, x):
+		return self.model(x)
+
 class LBPPatchedCNV2B(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
-		self.model = timm.create_model('convnextv2_base.fcmae_ft_in22k_in1k_384', pretrained=True)
+		self.model = timm.create_model("convnextv2_base.fcmae_ft_in22k_in1k", pretrained=True)
 		old_conv = self.model.stem[0]
 		self.model.stem[0] = nn.Conv2d(
 			in_channels=4,
