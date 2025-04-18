1180163f1b240f21621ac72245ab8a5527db38a5
Gregory <49094805+glimeuxe@users.noreply.github.com>
2025-04-18 11:12:10 +0000

    Add files via upload

--- architectures.py
+++ architectures.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 from collections import deque
+from torchvision.models import ResNet50_Weights, ResNet152_Weights, DenseNet201_Weights
 
 # Small units
 class SomnialUnit(nn.Module):
@@ -38,77 +39,13 @@ class SomnialUnit(nn.Module):
 		m = self.modulator(x_s_hat, x_t)
 		return m * x_s_hat + (1 - m) * x_t
 
-# Hybrid architectures
-class SR50ViTB16(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.id = self.__class__.__name__
-		self.backbone = timm.create_model("vit_base_r50_s16_224", pretrained=True, num_classes=0)
-		self.backbone_stem = self.backbone.patch_embed.backbone
-		in_channels = self.backbone_stem.feature_info[-1]['num_chs']
-		self.somnial = SomnialUnit(in_channels)
-		self.pool = nn.Identity()
-		self.classifier = nn.Linear(self.backbone.num_features, 2)
-
-	def forward(self, x):
-		x = self.backbone_stem(x)
-		x = self.somnial(x)
-		x = self.backbone.patch_embed.proj(x)
-		x = x.flatten(2).transpose(1, 2)
-		if hasattr(self.backbone, 'pos_drop'):
-			x = self.backbone.pos_drop(x + self.backbone.pos_embed[:, :x.size(1)])
-		for blk in self.backbone.blocks:
-			x = blk(x)
-		if hasattr(self.backbone, 'norm'):
-			x = self.backbone.norm(x)
-		x = x[:, 0]
-		x = self.pool(x)
-		x = self.classifier(x)
-		return x
-
-class R50ViTB16(nn.Module):
-	def __init__(self, num_classes=2):
-		super().__init__()
-		self.id = self.__class__.__name__
-		self.model = timm.create_model("vit_base_r50_s16_224", pretrained=True, num_classes=num_classes)
-
-	def forward(self, x):
-		return self.model(x)
-
-class CLIPViTL14(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.id = self.__class__.__name__
-		model, _, preprocess = open_clip.create_model_and_transforms("ViT-L-14", pretrained="openai")
-		self.model = model
-		self.preprocess = preprocess
-		self.tokenizer = open_clip.get_tokenizer("ViT-L-14")
-		self.prompts = ["an image of a nonrecyclable item", "an image of a recyclable item"]
-		self.text_tokens = self.tokenizer(self.prompts).to(DEVICE)
-
-		for param in self.model.parameters():
-			param.requires_grad = True
-
-# ViT architectures
-class ViTB16(nn.Module):
-	def __init__(self, num_classes=2):
-		super(ViTB16, self).__init__()
-		self.id = self.__class__.__name__
-		self.model = models.vit_b_16(pretrained=True)
-		self.model.heads = nn.Sequential(
-			nn.Linear(self.model.hidden_dim, num_classes)
-		)
-
-	def forward(self, x):
-		return self.model(x)
-
 # CNN architectures: ResNets
 class RN50(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
 		self.freezable = True
-		self.model = models.resnet50(pretrained=True)
+		self.model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
 		self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
 	def forward(self, x):
@@ -140,7 +77,7 @@ class RN152(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
-		self.model = models.resnet152(pretrained=True)
+		self.model = models.resnet152(weights=ResNet152_Weights.DEFAULT)
 		self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
 	def forward(self, x):
@@ -150,7 +87,7 @@ class LBPPatchedRN152(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
-		self.model = models.resnet152(pretrained=True)
+		self.model = models.resnet152(weights=ResNet152_Weights.DEFAULT)
 		old_conv = self.model.conv1
 		self.model.conv1 = nn.Conv2d(
 			in_channels=4,
@@ -168,76 +105,11 @@ class LBPPatchedRN152(nn.Module):
 	def forward(self, x):
 		return self.model(x)
 
-# CNN architectures: DenseNets
-class DN121(nn.Module):
-	def __init__(self, num_classes=2):
-		super().__init__()
-		self.id = self.__class__.__name__
-		self.model = models.densenet121(pretrained=True)
-		self.model.classifier = nn.Linear(self.model.classifier.in_features, num_classes)
-
-	def forward(self, x):
-		return self.model(x)
-
-class LBPPatchedDN121(nn.Module):
-	def __init__(self, num_classes=2):
-		super().__init__()
-		self.id = self.__class__.__name__
-		self.model = models.densenet121(pretrained=True)
-		old_conv = self.model.features.conv0
-		self.model.features.conv0 = nn.Conv2d(
-			in_channels=4,
-			out_channels=old_conv.out_channels,
-			kernel_size=old_conv.kernel_size,
-			stride=old_conv.stride,
-			padding=old_conv.padding,
-			bias=old_conv.bias is not None
-		)
-		with torch.no_grad():
-			self.model.features.conv0.weight[:, :3] = old_conv.weight
-			self.model.features.conv0.weight[:, 3] = old_conv.weight[:, 0]
-		self.model.classifier = nn.Linear(self.model.classifier.in_features, num_classes)
-
-	def forward(self, x):
-		return self.model(x)
-
-class DN169(nn.Module):
-	def __init__(self, num_classes=2):
-		super().__init__()
-		self.id = self.__class__.__name__
-		self.model = models.densenet169(pretrained=True)
-		self.model.classifier = nn.Linear(self.model.classifier.in_features, num_classes)
-
-	def forward(self, x):
-		return self.model(x)
-
-class LBPPatchedDN169(nn.Module):
-	def __init__(self, num_classes=2):
-		super().__init__()
-		self.id = self.__class__.__name__
-		self.model = models.densenet169(pretrained=True)
-		old_conv = self.model.features.conv0
-		self.model.features.conv0 = nn.Conv2d(
-			in_channels=4,
-			out_channels=old_conv.out_channels,
-			kernel_size=old_conv.kernel_size,
-			stride=old_conv.stride,
-			padding=old_conv.padding,
-			bias=old_conv.bias is not None
-		)
-		with torch.no_grad():
-			self.model.features.conv0.weight[:, :3] = old_conv.weight
-			self.model.features.conv0.weight[:, 3] = old_conv.weight[:, 0]
-		self.model.classifier = nn.Linear(self.model.classifier.in_features, num_classes)
-
-	def forward(self, x):
-		return self.model(x)
-
 class DN201(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
-		self.model = models.densenet201(pretrained=True)
+		self.model = models.densenet201(weights=DenseNet201_Weights.DEFAULT)
 		self.model.classifier = nn.Linear(self.model.classifier.in_features, num_classes)
 
 	def forward(self, x):
@@ -247,7 +119,7 @@ class LBPPatchedDN201(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
-		self.model = models.densenet201(pretrained=True)
+		self.model = models.densenet201(weights=DenseNet201_Weights.DEFAULT)
 		old_conv = self.model.features.conv0
 		self.model.features.conv0 = nn.Conv2d(
 			in_channels=4,
@@ -265,7 +137,6 @@ class LBPPatchedDN201(nn.Module):
 	def forward(self, x):
 		return self.model(x)
 
-# CNN architectures: Inceptions
 class IV3(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
@@ -285,13 +156,34 @@ class IV4(nn.Module):
 	def forward(self, x):
 		return self.model(x)
 
-# CNN architectures: MobileNets
 class MNV2(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
 		self.model = models.mobilenet_v2(pretrained=True)
 		self.model.classifier[1] = nn.Linear(self.model.last_channel, num_classes)
+
+	def forward(self, x):
+		return self.model(x)
+
+class LBPPatchedMNV2(nn.Module):
+	def __init__(self, num_classes=2):
+		super().__init__()
+		self.id = self.__class__.__name__
+		self.model = models.mobilenet_v2(pretrained=True)
+		old_conv = self.model.features[0][0]
+		self.model.features[0][0] = nn.Conv2d(
+			in_channels=4,
+			out_channels=old_conv.out_channels,
+			kernel_size=old_conv.kernel_size,
+			stride=old_conv.stride,
+			padding=old_conv.padding,
+			bias=old_conv.bias is not None
+		)
+		with torch.no_grad():
+			self.model.features[0][0].weight[:, :3] = old_conv.weight
+			self.model.features[0][0].weight[:, 3] = old_conv.weight[:, 0]
+		self.model.classifier[1] = nn.Linear(self.model.classifier[1].in_features, num_classes)
 
 	def forward(self, x):
 		return self.model(x)
@@ -316,22 +208,29 @@ class MNV3L(nn.Module):
 	def forward(self, x):
 		return self.model(x)
 
-# Experiments
-class CNV2B1(nn.Module):
+class LBPPatchedMNV3L(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
 		self.id = self.__class__.__name__
-		self.model = timm.create_model("convnextv2_base.fcmae_ft_in22k_in1k", pretrained=True, num_classes=0)
-		self.pool = nn.AdaptiveAvgPool2d((1, 1))
-		self.head = nn.Linear(self.model.num_features, num_classes)
+		self.model = models.mobilenet_v3_large(pretrained=True)
+		old_conv = self.model.features[0][0]
+		self.model.features[0][0] = nn.Conv2d(
+			in_channels=4,
+			out_channels=old_conv.out_channels,
+			kernel_size=old_conv.kernel_size,
+			stride=old_conv.stride,
+			padding=old_conv.padding,
+			bias=old_conv.bias is not None
+		)
+		with torch.no_grad():
+			self.model.features[0][0].weight[:, :3] = old_conv.weight
+			self.model.features[0][0].weight[:, 3] = old_conv.weight[:, 0]
+		self.model.classifier[3] = nn.Linear(self.model.classifier[3].in_features, num_classes)
 
 	def forward(self, x):
-		x = self.model(x)
-		if x.ndim == 4:
-			x = self.pool(x)
-			x = x.view(x.size(0), -1)
-		return self.head(x)
+		return self.model(x)
 
+# Experiments
 class EVA2L(nn.Module):
 	def __init__(self, num_classes=2):
 		super().__init__()
@@ -359,50 +258,6 @@ class LBPPatchedEVA2L(nn.Module):
 			self.model.patch_embed.proj.weight[:, :3] = old_proj.weight
 			self.model.patch_embed.proj.weight[:, 3] = old_proj.weight[:, 0]
 		self.model.head = nn.Linear(self.model.head.in_features, num_classes)
-
-	def forward(self, x):
-		return self.model(x)
-
-class LBPPatchedMNV2(nn.Module):
-	def __init__(self, num_classes=2):
-		super().__init__()
-		self.id = self.__class__.__name__
-		self.model = models.mobilenet_v2(pretrained=True)
-		old_conv = self.model.features[0][0]
-		self.model.features[0][0] = nn.Conv2d(
-			in_channels=4,
-			out_channels=old_conv.out_channels,
-			kernel_size=old_conv.kernel_size,
-			stride=old_conv.stride,
-			padding=old_conv.padding,
-			bias=old_conv.bias is not None
-		)
-		with torch.no_grad():
-			self.model.features[0][0].weight[:, :3] = old_conv.weight
-			self.model.features[0][0].weight[:, 3] = old_conv.weight[:, 0]
-		self.model.classifier[1] = nn.Linear(self.model.classifier[1].in_features, num_classes)
-
-	def forward(self, x):
-		return self.model(x)
-
-class LBPPatchedMNV3(nn.Module):
-	def __init__(self, num_classes=2):
-		super().__init__()
-		self.id = self.__class__.__name__
-		self.model = models.mobilenet_v3_large(pretrained=True)
-		old_conv = self.model.features[0][0]
-		self.model.features[0][0] = nn.Conv2d(
-			in_channels=4,
-			out_channels=old_conv.out_channels,
-			kernel_size=old_conv.kernel_size,
-			stride=old_conv.stride,
-			padding=old_conv.padding,
-			bias=old_conv.bias is not None
-		)
-		with torch.no_grad():
-			self.model.features[0][0].weight[:, :3] = old_conv.weight
-			self.model.features[0][0].weight[:, 3] = old_conv.weight[:, 0]
-		self.model.classifier[3] = nn.Linear(self.model.classifier[3].in_features, num_classes)
 
 	def forward(self, x):
 		return self.model(x)
